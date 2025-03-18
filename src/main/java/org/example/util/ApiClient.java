5ad0d850f3ad9fbba1027effc4ab0f9ff3549bc1
RossNeyman <ross.neyman@gmail.com>
2025-03-18 18:07:19 -0400

    implemented round robin for api keys

--- src/main/java/org/example/util/ApiClient.java
+++ src/main/java/org/example/util/ApiClient.java
@@ -10,11 +10,17 @@ import java.net.URL;
 
 public class ApiClient {
     private static final Dotenv dotenv = Dotenv.load();
-    private static final String POLYGON_API_KEY = dotenv.get("POLYGON_API_KEY");
-
+    private static final String[] POLYGON_API_KEYS = {
+            dotenv.get("POLYGON_API_KEY_1"),
+            dotenv.get("POLYGON_API_KEY_2"),
+            dotenv.get("POLYGON_API_KEY_3"),
+            dotenv.get("POLYGON_API_KEY_4")
+    };
+    private static int numberOfRequests = 0;
 
 
     public static String fetchStockData(String apiCall) throws IOException {
+        ApiClient.numberOfRequests++;
         URL apiCallUrl = new URL(apiCall);
         HttpURLConnection apiConnection = (HttpURLConnection) apiCallUrl.openConnection();
         int httpResponse = apiConnection.getResponseCode();
@@ -38,8 +44,11 @@ public class ApiClient {
 
     public static @NotNull String getGeneralStockData(String tickerSymbol) throws IOException {
         return fetchStockData(String.format("https://api.polygon.io/v3/reference/tickers/%s?apiKey=%s",
-                tickerSymbol, POLYGON_API_KEY
+                tickerSymbol, getApiKey()
         ));
+    }
+    private static String getApiKey(){
+        return POLYGON_API_KEYS[numberOfRequests % 4];
     }
 
 }
--- src/main/java/org/example/util/TickerValidator.java
+++ src/main/java/org/example/util/TickerValidator.java
@@ -1,20 +1,17 @@
 package org.example.util;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
-import java.io.File;
 
 public class TickerValidator {
     public static void main(String[] args) {
 
             List<String> validSymbols = new ArrayList<>();
             String resourcePath = "tickers/top100tickers";
+            int tickersValidated = 0;
 
             try (InputStream inputStream = TickerValidator.class.getClassLoader().getResourceAsStream(resourcePath)) {
                 assert inputStream != null;
@@ -24,8 +21,8 @@ public class TickerValidator {
                     while ((line = reader.readLine()) != null) {
                         if((Objects.requireNonNull(SerDe.deserializeJsonStockResponse(ApiClient.getGeneralStockData(line.trim())))).getListDate() != null)
                             validSymbols.add(line.trim());
-                        System.out.println("Checked symbol. Waiting for api...");
-                        TimeUnit.SECONDS.sleep(15);
+                        System.out.println("Checked symbol. Waiting for api..." + tickersValidated++);
+                        TimeUnit.SECONDS.sleep(2);
                     }
                 }
             }
@@ -33,8 +30,18 @@ public class TickerValidator {
                 System.out.println(e.getMessage());
             }
 
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter("tickers/validTickers"))) {
+                for (String symbol : validSymbols) {
+                    writer.write(symbol);
+                    writer.newLine();
+                }
+                System.out.println("Valid tickers written to tickers/validTickers");
+            } catch (IOException e) {
+                System.out.println("Error writing to file: " + e.getMessage());
+            }
 
-            System.out.println(validSymbols.size());
+
+        System.out.println(validSymbols.size());
 
     }
 }
