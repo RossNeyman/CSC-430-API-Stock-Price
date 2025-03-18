93cd35a38425413dbb633a28d04879951d18f4ff
RossNeyman <ross.neyman@gmail.com>
2025-03-18 11:31:02 -0400

    game runs

--- src/main/java/org/example/ListDateNameGame.java
+++ src/main/java/org/example/ListDateNameGame.java
@@ -3,9 +3,11 @@ import org.example.util.ApiClient;
 import org.example.util.SerDe;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ListDateNameGame {
@@ -36,11 +38,25 @@ public class ListDateNameGame {
         return dates;
     }
 
-    public void playGame() throws IOException {
-        Stock gameStock = SerDe.deserializeJsonStockResponse(ApiClient.getGeneralStockData("AAPL"));
+    public static boolean playGame(String ticker) throws IOException {
+        Stock gameStock = SerDe.deserializeJsonStockResponse(ApiClient.getGeneralStockData(ticker));
         assert gameStock != null;
-        for(String date: generateRandomDates(gameStock.getListDate(), 5)){
-            System.out.println(date);
+
+        Scanner gameScanner = new Scanner(System.in);
+        String correctListDate = gameStock.getListDate();
+
+        ListDateNameGame game = new ListDateNameGame();
+        String[] options = game.generateRandomDates(correctListDate, 4);
+        //System.out.println("You chose " + ticker + ": " + gameStock.getName());
+        System.out.println("Guess the correct listing date for the stock: " + ticker);
+        for (int i = 0; i < options.length; i++) {
+            System.out.println((i + 1) + ". " + options[i]);
         }
+        System.out.print("Enter the number corresponding to your choice: ");
+        int userChoice = gameScanner.nextInt();
+
+        return options[userChoice - 1].equals(correctListDate);
     }
+
 }
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,42 @@ package org.example;
 
 import org.example.util.ApiClient;
 import org.example.util.SerDe;
+import java.util.Scanner;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        ListDateNameGame game = new ListDateNameGame();
-        game.playGame();
+        Scanner scanner = new Scanner(System.in);
+        boolean quitGame = false;
+        int wins = 0;
+        while(!quitGame) {
+            System.out.println("You've won " + wins + " times.");
+            System.out.println("Enter the stock ticker symbol: ");
+            String ticker = scanner.nextLine();
+
+            try {
+                boolean result = ListDateNameGame.playGame(ticker.toUpperCase());
+                if (result) {
+                    System.out.println("Congratulations! You won!");
+                    wins++;
+                } else {
+                    System.out.println("Better luck next time!");
+                }
+                System.out.println(
+                        "Would you like to play again?" + '\n'
+                        +"1. Play again" + '\n'
+                        +"2. Quit"
+                );
+                if(scanner.nextInt() != 1){
+                    quitGame = true;
+                }
+                scanner.nextLine();
+            } catch (IOException e) {
+                System.out.println("An error occurred while retrieving stock data.");
+                e.printStackTrace();
+            }
+        }
     }
+
 }
